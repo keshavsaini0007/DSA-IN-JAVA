7654fb9db8649f7e9b3b07941e1b1285485ea23d
keshav saini <sainikeshav241@gmail.com>
2025-12-08 01:15:52 +0530

    Delete function 2 directory

--- function 2/circumferenc_of_circle.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class circumferenc_of_circle {
-    public static double area(double r){   return 2*(3.1415926)*r;  }
-    public static void main(String[]args){
-    Scanner scan = new Scanner(System.in);
-    double r = scan.nextDouble();
-    double ar = area(r);
-    System.out.println(ar);
-}}
