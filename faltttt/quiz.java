0b16c9e524579160dc8cb315510fa28a9d2a7617
keshav saini <sainikeshav241@gmail.com>
2025-12-13 18:58:04 +0530

    push

---
+++ faltttt/quiz.java
@@ -0,0 +1,16 @@
+public class quiz {
+    public static void main (String [] args ){
+        // int x = 83;
+        // double d = 34.3;
+        // int f = 76;
+        // int e = 83;
+        // double  ans  =   (x+d)/(f+e);
+        // System.out.println(ans);
+
+        // int x = 12; 
+        // double d = 13;
+        // int f = x/d;
+        // System.out.println(f);
+
+    }
+}
