b1f885a76295cc36bb2197de7d67224ffc72c802
keshav saini <sainikeshav241@gmail.com>
2025-12-13 18:56:22 +0530

    push

---
+++ faltttt/basic.java
@@ -0,0 +1,10 @@
+public class basic {
+    public static void main (String [] args){
+        int X = 767690;
+        int z = 767690;
+        String name = "keshav saini ";
+        double price = 76.4;
+        System.out.println(name);
+        System.out.println(X +z+ price);
+    }
+}
