8db14a9c4e8960c3d6fe0c1cdc10c10b3d4bb1e8
keshav saini <sainikeshav241@gmail.com>
2026-02-05 17:04:04 +0530

    push

---
+++ do_while/basic.java
@@ -0,0 +1,12 @@
+// import java.util.*;
+
+public class basic {
+    public static void main (String[]args){
+        // Scanner scan = new Scanner (System.in);
+        int x = 01;
+        do{
+        System.out.println("yes");
+        break; }  // break --> for one time loop
+        while(x>0);
+    }
+}
