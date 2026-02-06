970183874d340c4a20e2ca800545fec7b0edd13e
keshav saini <sainikeshav241@gmail.com>
2026-02-06 14:24:27 +0530

    push

---
+++ oprators/arithmatic_operators.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class arithmatic_operators {
+    public static void main (String[]args){
+        Scanner scan = new Scanner(System.in);
+        int i = 1 ; 
+        System.out.println(i++);    
+        System.out.println(i);
+        
+        System.out.println();
+
+        int j = 1; 
+        System.out.println(++j);
+        System.out.println(j);
+
+        System.out.println();
+
+        int k = 1;
+        System.out.println(k--);
+        System.out.println(k);
+        
+        System.out.println();
+
+        int l = 1;
+        System.out.println(--l);
+        System.out.println(l);
+
+        System.out.println();
+
+    }
+}
