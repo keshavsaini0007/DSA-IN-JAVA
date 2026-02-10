166e41be905688a90afeb021321abea709923fcf
keshav saini <sainikeshav241@gmail.com>
2026-02-10 18:39:38 +0530

    push

---
+++ oprators/assignment_operator.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class assignment_operator {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        
+        int x = 1;
+        x+=1;
+        System.out.println(x);
+
+        int y = 1;
+        y-=1;
+        System.out.println(y);
+
+        int z = 1;
+        z*=2;
+        System.out.println(z);
+
+        int zz = 1;
+        zz/=2;
+        System.out.println(zz);
+    }
+}
