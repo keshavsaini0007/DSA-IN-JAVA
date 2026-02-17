ae4e17f4eb261b2a3f54acbd22e1d3a74ef8336a
keshav saini <sainikeshav241@gmail.com>
2026-02-17 20:45:11 +0530

    push

---
+++ sorting/bubble_sort.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class bubble_sort {
+  public static void print(int arr[]){
+     for(int i=0; i<arr.length; i++){
+      System.out.print(arr[i]+" ");
+     }
+  }
+   public static void main(String[]args){
+     int arr[] = {4,2,8,5,3,1};
+     
+     for(int i = 0; i<(arr.length-1); i++){
+        for(int j = 0; j<(arr.length-1-i); j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+      
+            } 
+        }
+     }
+     print(arr);               // {1,2,3,4,5,8}
+   } 
+}
