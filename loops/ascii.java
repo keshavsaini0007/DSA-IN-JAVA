544d518645c7fd6e2a7067de46f4d6e87199979b
keshav saini <sainikeshav241@gmail.com>
2026-03-26 21:04:33 +0530

    ascii

---
+++ loops/ascii.java
@@ -0,0 +1,10 @@
+public class ascii {
+    public static void main (String [] args){
+        int c = 1;
+        do{
+            System.out.print((char) c);
+            System.out.print("  ");
+            c++;
+        } while (c<=225);
+    }
+}
