e978960935d6ce34ecf8b30eaa69047abe040d11
keshav saini <sainikeshav241@gmail.com>
2026-03-27 18:42:01 +0530

    alphabet

---
+++ loops/alphabet_from_A_to_Z.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class alphabet_from_A_to_Z {
+public static void main (String []args ){
+    Scanner scan = new Scanner (System.in);
+    char c = 65;
+for (int i = 66 ; i<=91; i++){
+
+    System.out.print(c+"  ");
+    c++;
+}
+}
+
+    static class Ascii_Values
+    {
+        public static void main(String[] args)
+        {
+            for(int s=1;s<=255;s++)
+            {
+                System.out.println((char)s);
+            }
+
+        }
+    }
+}
